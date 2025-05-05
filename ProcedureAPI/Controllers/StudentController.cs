a53bd01b706d5489ac51f917f1379a194cc7c582
Xin chao mn <mactrung116@gmail.com>
2025-05-05 10:32:17 +0700

    firstblood

---
+++ ProcedureAPI/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ProcedureAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public StudentController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Student> std = new List<Student>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("GetAll", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    std.Add(new Student
+                    {
+                        Id = (int)reader["Id"],
+                        StudentName = reader["StudentName"].ToString(),
+                        Age = (int)reader["Age"],
+                        Address = reader["Address"].ToString(),
+                        PhoneNumber = reader["PhoneNumber"].ToString()
+                    });
+                }
+            }
+            return Ok(std);
+        }
+    }
+}
---
+++ ProcedureAPI/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcedureAPI
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; } = String.Empty;
+        [MaxLength(20)]
+        public string PhoneNumber { get; set; }
+
+    }
+}
